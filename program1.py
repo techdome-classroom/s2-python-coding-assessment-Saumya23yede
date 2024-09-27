eb25579bef00a2943827812cb80e7a88b156f302
Saumya23yede <saumyayede05@gmail.com>
2024-09-27 13:57:11 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,20 +6,7 @@
 #         """
 #         pass
 
-def isValid(s: str) -> bool:
-    leftSymbols = []
-    for c in s:
-        if c in ['(', '{', '[']:
-            leftSymbols.append(c)
-        elif c == ')' and len(leftSymbols) != 0 and leftSymbols[-1] == '(':
-            leftSymbols.pop()
-        elif c == '}' and len(leftSymbols) != 0 and leftSymbols[-1] == '{':
-            leftSymbols.pop()
-        elif c == ']' and len(leftSymbols) != 0 and leftSymbols[-1] == '[':
-            leftSymbols.pop()
-        else:
-            return False
-    return leftSymbols == []
+
 
 
 
