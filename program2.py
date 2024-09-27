a3eac285ee99a1ff1319d5ec1f742abff15415ce
Saumya23yede <saumyayede05@gmail.com>
2024-09-27 14:09:54 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -32,8 +32,8 @@ class Solution(object):
         return total
 
 solution = Solution()
-print(solution.romanToInt("III"))       # Output: 3
-print(solution.romanToInt("LVIII"))     # Output: 58
+print(solution.romanToInt("III"))      
+print(solution.romanToInt("LVIII"))     
 print(solution.romanToInt("MCMXCIV"))   # Output: 1994
 
 
