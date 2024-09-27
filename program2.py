cb2e1b8e5e2bb0c7c7bbbf9a6cb1f6cd35967563
Saumya23yede <saumyayede05@gmail.com>
2024-09-27 14:05:01 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,5 +6,40 @@ class Solution(object):
         """
         pass
 
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # Dictionary to store Roman numerals and their corresponding integer values
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        # Initialize total to 0
+        total = 0
+        # Traverse through the string
+        for i in range(len(s)):
+            # If this is not the last character and the current value is less than the next value, subtract it
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                # Otherwise, add the current value to total
+                total += roman_map[s[i]]
+        
+        return total
+
+# Test cases
+solution = Solution()
+print(solution.romanToInt("III"))       # Output: 3
+print(solution.romanToInt("LVIII"))     # Output: 58
+print(solution.romanToInt("MCMXCIV"))   # Output: 1994
 
 
